6427c10b2d75ea328fe009f389046392df2dd9b2
Abhinav Tripathi <abhinav.tripathi@publicissapient.com>
2023-08-25 16:52:26 +0530

    Build issue fix

--- src/components/pages/Contact/Contact.tsx
+++ src/components/pages/Contact/Contact.tsx
@@ -25,7 +25,6 @@ const Contact = () => {
                         id="email"
                         className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                         placeholder="name@flowbite.com"
-                        required=""
                         />
                     </div>
                     <div>
@@ -40,7 +39,6 @@ const Contact = () => {
                         id="subject"
                         className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                         placeholder="Let us know how we can help you"
-                        required=""
                         />
                     </div>
                     <div className="sm:col-span-2">
